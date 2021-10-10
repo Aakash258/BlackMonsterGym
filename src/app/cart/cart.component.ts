8f718d3548daefd80fc560bb7b3cacf737b8dad7
Aniket Dwivedi <dwivedianiket98@gmail.com>
2021-10-10 17:33:50 +0530

    Cart Total Working Now

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,11 +7,11 @@ import { CrudService } from '../service/crud.service';
   styleUrls: ['./cart.component.css']
 })
 
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit {
 
-  constructor(private crud: CrudService) {}
+  constructor(private crud: CrudService) { }
 
-  ngOnInit(){
+  ngOnInit() {
 
     this.crud.getProducts().subscribe(res => {
       this.products = res,
@@ -21,26 +21,28 @@ export class CartComponent implements OnInit{
     // this.populateCart()
   }
 
-  products:any
-  productsID:any=[]
+  products: any
+  productsID: any = []
 
   populateCart() {
     console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     let mycart = JSON.parse(sessionStorage.getItem('mycart') || '[]')
     console.log('cart component: ', mycart)
 
-    var currCart:any=[]
+    var currCart: any = []
 
-    for(let i=0; i<this.products.length; i++) {
+    let totalPrice:number = 0;
+    for (let i = 0; i < this.products.length; i++) {
       this.productsID.push(this.products[i].payload.doc.id)
       // console.log(this.products[i].payload.doc.id)
 
-      if(mycart.includes(this.products[i].payload.doc.id)) {
+      if (mycart.includes(this.products[i].payload.doc.id)) {
+        totalPrice += this.products[i].payload.doc.data().price
         currCart.push(this.products[i])
       }
     }
-    
-    return currCart
+
+    return [currCart, totalPrice]
 
   }
 
